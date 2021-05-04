b315d9047d8ffa162ac62729f52449d4ba6dbf6f
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-04 20:44:51 +0530

    refactor : updated reducer

--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -20,63 +20,10 @@ const reducer = (state, action) => {
                 ...state,
                 playlists: action.payload,
             };
-        case "SET_PLAYLIST_NAME":
+        case "UPDATE_PLAYLISTS":
             return {
                 ...state,
-                playListName: action.payload,
-            };
-        case "CREATE_PLAYLIST":
-            return {
-                ...state,
-                playList: [
-                    ...state.playList,
-                    {
-                        id: state.playListName,
-                        name: state.playListName,
-                        createdBy: "You",
-                        videos: [],
-                    },
-                ],
-            };
-        case "DELETE_PLAYLIST":
-            return {
-                ...state,
-                playList: state.playList.filter(
-                    (item) => item.id !== action.payload
-                ),
-            };
-        case "ADD_TO_PLAYLIST":
-            return {
-                ...state,
-                playList: [
-                    ...state.playList.filter(
-                        (playList) =>
-                            playList.id !== action.payload.currPlayList.id
-                    ),
-                    {
-                        ...action.payload.currPlayList,
-                        videos: [
-                            ...action.payload.currPlayList.videos,
-                            action.payload.currVideo,
-                        ],
-                    },
-                ],
-            };
-        case "REMOVE_FROM_PLAYLIST":
-            return {
-                ...state,
-                playList: [
-                    ...state.playList.filter(
-                        (playList) =>
-                            playList.id !== action.payload.currPlayList.id
-                    ),
-                    {
-                        ...action.payload.currPlayList,
-                        videos: action.payload.currPlayList.videos.filter(
-                            (video) => video.id !== action.payload.currVideo.id
-                        ),
-                    },
-                ],
+                playlists: [...state.playlists, action.payload],
             };
         case "SET_SUBSCRIPTIONS":
             return {

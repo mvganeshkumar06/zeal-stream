dc6d601fcbc1b4c0787e1e0ea5d272c6d0a0c0ea
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-05 21:49:07 +0530

    refactor : updated router

--- src/router/Router.js
+++ src/router/Router.js
@@ -6,8 +6,10 @@ import {
     Library,
     Login,
     Playlist,
+    Podcast,
     Podcasts,
     Subscriptions,
+    Video,
     Videos,
 } from "../pages";
 import PrivateRoute from "./PrivateRoute";
@@ -27,8 +29,14 @@ const Router = () => {
                 <PrivateRoute path="/library">
                     <Library />
                 </PrivateRoute>
+                <Route path="/podcasts/:podcastId">
+                    <Podcast />
+                </Route>
                 <Route path="/podcasts">
                     <Podcasts />
+                </Route>
+                <Route path="/videos/:videoId">
+                    <Video />
                 </Route>
                 <Route path="/videos">
                     <Videos />

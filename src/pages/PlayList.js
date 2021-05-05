5c5f44cb46d21d0d62371f4320ce9271ea6f8920
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-05 21:48:21 +0530

    refactor : updated page

--- src/pages/PlayList.js
+++ src/pages/PlayList.js
@@ -1,12 +1,49 @@
 import React from "react";
-import { Container, Grid, Text } from "@zeal-ui/core";
+import {
+    Container,
+    Grid,
+    Text,
+    Divider,
+    useNotify,
+    List,
+    ListItem,
+    Menu,
+} from "@zeal-ui/core";
 import { Video, Podcast } from "../components";
 import useStreamContext from "../hooks/useStreamContext";
 import { useParams } from "react-router";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 const PlayList = () => {
     const styles = `    
-        margin: 8rem 0rem;
+        margin: 8rem 5%;
+
+        .playlistHeader{
+            margin-bottom:5rem;
+        }
+
+        .playlistNameContainer{
+            position:relative;
+        }
+
+        .separator{
+            width:0.5rem;
+            height:0.5rem;
+            margin:0rem 0.5rem;
+        }
+       
+        .moreIcon{
+            margin-left:1rem;
+        }
+
+        .moreIcon:hover{
+            cursor:pointer;
+        }
+
+        .playlistDivider{
+            margin:2rem 0rem;
+        }
 
         .streamContainer {
             width: 100%;
@@ -42,43 +79,71 @@ const PlayList = () => {
 
     const { name, description, podcasts, videos } = playlist;
 
+    const { isOpen, onOpen } = useNotify();
+
     return (
-        <Container type="col" rowCenter customStyles={styles}>
-            <Text type="mainHeading">{name}</Text>
-            {description && <Text>{description}</Text>}
-            {podcasts.length > 0 && (
-                <>
-                    <Text type="subHeading">Podcasts</Text>
-                    <Container type="col" rowCenter width="90%">
-                        <Grid className="streamContainer">
-                            {podcasts.map((podcast) => {
-                                return (
-                                    <Podcast
-                                        podcastDetails={podcast}
-                                        key={podcast._id}
-                                    />
-                                );
-                            })}
-                        </Grid>
-                    </Container>
-                </>
+        <Container type="col" customStyles={styles}>
+            <Container type="col" width="100%" className="playlistHeader">
+                <Container
+                    type="row"
+                    width="100%"
+                    colCenter
+                    className="playlistNameContainer"
+                >
+                    <Text type="mainHeading">{name}</Text>
+                    <MoreVertIcon
+                        className="moreIcon"
+                        onClick={() => onOpen("MENU")}
+                    />
+                    <Menu
+                        isOpen={isOpen === "MENU"}
+                        top="3.25rem"
+                        left="12.25rem"
+                    >
+                        <List type="link">
+                            <ListItem>Edit playlist name</ListItem>
+                            <ListItem>Edit playlist description</ListItem>
+                        </List>
+                    </Menu>
+                </Container>
+                <Container type="row" colCenter>
+                    <Text>{podcasts.length} podcast(s)</Text>
+                    <FiberManualRecordIcon className="separator" />
+                    <Text> {videos.length} video(s)</Text>
+                </Container>
+                <Container type="row" colCenter>
+                    <Text>
+                        {description.length > 0
+                            ? description
+                            : "No playlist description"}
+                    </Text>
+                </Container>
+            </Container>
+            <Text type="subHeading">Podcasts</Text>
+            {podcasts.length > 0 ? (
+                <Grid className="streamContainer">
+                    {podcasts.map((podcast) => {
+                        return (
+                            <Podcast
+                                podcastDetails={podcast}
+                                key={podcast._id}
+                            />
+                        );
+                    })}
+                </Grid>
+            ) : (
+                <Text>No podcasts in this playlist yet</Text>
             )}
-            {videos.length > 0 && (
-                <>
-                    <Text type="subHeading">Videos</Text>
-                    <Container type="col" rowCenter width="90%">
-                        <Grid className="streamContainer">
-                            {videos.map((video) => {
-                                return (
-                                    <Video
-                                        videoDetails={video}
-                                        key={video._id}
-                                    />
-                                );
-                            })}
-                        </Grid>
-                    </Container>
-                </>
+            <Divider className="playlistDivider" />
+            <Text type="subHeading">Videos</Text>
+            {videos.length > 0 ? (
+                <Grid className="streamContainer">
+                    {videos.map((video) => {
+                        return <Video videoDetails={video} key={video._id} />;
+                    })}
+                </Grid>
+            ) : (
+                <Text>No videos in this playlist yet</Text>
             )}
         </Container>
     );

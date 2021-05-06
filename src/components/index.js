c703a1f65524f1f027446468c20d01546a16cfaf
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-06 11:16:26 +0530

    updated import

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { default as App } from "./App";
-export { default as Header } from "./Header.js";
-export { default as Navigation } from "./Navigation.js";
-export { default as PlaylistModal } from "./PlaylistModal.js";
+export { default as Header } from "./Header";
+export { default as Navigation } from "./Navigation";
+export { default as PlaylistModal } from "./PlaylistModal";
 export { default as Podcast } from "./Podcast";
-export { default as Video } from "./Video.js";
+export { default as Video } from "./Video";

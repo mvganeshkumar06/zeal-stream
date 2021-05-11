76100b122432aae609ab2d779dd8e89c6e8033c9
Ganesh Kumar <mvganeshk15sde@gmail.com>
2021-05-11 09:44:58 +0530

    refactor : updated navigation items

--- src/utils/NavigationItems.js
+++ src/utils/NavigationItems.js
@@ -1,23 +1,39 @@
+import HomeIcon from "@material-ui/icons/Home";
+import MicIcon from "@material-ui/icons/Mic";
+import VideocamIcon from "@material-ui/icons/Videocam";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+
 const navigationItems = [
     {
         id: 1,
         name: "Home",
         url: "/",
+        icon: <HomeIcon />,
     },
     {
         id: 2,
-        name: "Library",
-        url: "/library",
+        name: "Podcasts",
+        url: "/podcasts",
+        icon: <MicIcon />,
     },
     {
         id: 3,
-        name: "Subscriptions",
-        url: "/subscriptions",
+        name: "Videos",
+        url: "/videos",
+        icon: <VideocamIcon />,
     },
     {
         id: 4,
-        name: "Login",
-        url: "/login",
+        name: "Library",
+        url: "/library",
+        icon: <VideoLibraryIcon />,
+    },
+    {
+        id: 5,
+        name: "Subscriptions",
+        url: "/subscriptions",
+        icon: <SubscriptionsIcon />,
     },
 ];
 
